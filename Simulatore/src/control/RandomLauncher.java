a6c58d7be18eee3f58f96922d8f78e4302fa0f0b
alemon85 <alemon85@f004985d-f06b-b5b5-9b68-7c53d483263a>
2010-06-10 08:53:37 +0000

    RandomLauncher developed.

--- Simulatore/src/control/RandomLauncher.java
+++ Simulatore/src/control/RandomLauncher.java
@@ -1,13 +1,34 @@
 package control;
 
+import org.apache.commons.math.random.MersenneTwister;
+
 public class RandomLauncher {
 
+	private static final int TRIES=1000;
+		
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-
+		int tries;
+		MersenneTwister rnd=new MersenneTwister();
+		double[][] table;			
+		
+		if (args.length!=0)
+			tries=Integer.parseInt(args[0]);
+		else
+			tries=TRIES;
+		
+		table = new double[tries][2];
+		
+		for (int i=0;i<tries;i++){
+			table[i][0]=rnd.nextDouble();
+			table[i][1]=rnd.nextDouble();
+		}
+		Utility.createCSVFile("scatterplot", null, table);
+		System.out.println(tries+" points generated.");
+		System.out.println("Data successfully saved on file 'scatterplot.cvs'.");
 	}
 
 }
